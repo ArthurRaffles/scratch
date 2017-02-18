4798c66f598eccda1f45ccccd5f84c5adad70501
David-Lappy <david@David-Lappys-MacBook-Pro.local>
2017-02-18 18:15:17 +0000

    babel scratchpad

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # scratch
+node_modules/.bin/babel destructors.js --presets=babel-preset-es2015 | node
---
+++ destructors.js
@@ -0,0 +1,13 @@
+
+
+class foo{
+	constructor(t){
+		console.log(t)
+	}
+}
+
+const t = {
+	name : "bob",
+	age: 22
+}
+const y = new foo(t)
